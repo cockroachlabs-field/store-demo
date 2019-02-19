08b19369731871059cee745c1a94d49981f72231
Tim Veil <tjveil@gmail.com>
2019-02-19 07:00:02 -0500

    initial commit

---
+++ ui/src/main/java/io/crdb/demo/store/StoreDemoApplication.java
@@ -0,0 +1,14 @@
+package io.crdb.demo.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDemoApplication.class, args);
+	}
+
+}
+
