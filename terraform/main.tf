4d55d2528866ecf3e3a8bfab774b2e34d167677a
Tim Veil <tjveil@gmail.com>
2019-02-26 11:53:10 -0500

    got multi region in google working

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,35 +3,41 @@ provider "google" {
   project = "cockroach-tv"
 }
 
+provider "google" {
+  alias = "east"
+  region = "us-east1"
+  zone = "us-east1-b"
+  project = "cockroach-tv"
 
-resource "google_compute_instance" "google_cockroach" {
-
-  count = 3
-
-  name = "crdb-gcp-${count.index}"
-  machine_type = "n1-standard-4"
-  zone = "us-east4-a"
-
-  tags = ["cockroach"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-      type = "pd-ssd"
-      size = "350"
-    }
-  }
-
-  metadata_startup_script = "${file("${path.module}/scripts/install.sh")}"
-
-  network_interface {
-    network = "${google_compute_network.google_vpc_network.self_link}"
-
-    access_config {
-      // Include this section to give the VM an external ip address
-    }
-  }
+  credentials = "${file("gcp-account.json")}"
 }
+
+provider "google" {
+  alias = "west"
+  region = "us-west2"
+  zone = "us-west2-b"
+  project = "cockroach-tv"
+
+  credentials = "${file("gcp-account.json")}"
+}
+
+provider "google" {
+  alias = "central"
+  region = "us-central1"
+  zone = "us-central1-b"
+  project = "cockroach-tv"
+
+  credentials = "${file("gcp-account.json")}"
+}
+
+variable "providers" {
+  type = "list"
+  default = ["google.east", "google.west", "google.central"]
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# setup gcp network
+# ---------------------------------------------------------------------------------------------------------------------
 
 resource "google_compute_network" "google_vpc_network" {
   name = "crdb-network"
@@ -74,26 +80,106 @@ resource "google_compute_firewall" "google_ssh" {
   target_tags = ["cockroach"]
 }
 
-resource "null_resource" "google_start_cluster" {
+# ---------------------------------------------------------------------------------------------------------------------
+# provision gcp instances
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_compute_instance" "google_east_cockroach" {
 
   count = 3
+
+  name = "crdb-gcp-east-${count.index}"
+  machine_type = "n1-standard-4"
+  provider = "google.east"
+
+  tags = ["cockroach"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+      size = "350"
+    }
+  }
+
+  scratch_disk {}
+
+  metadata_startup_script = "${file("${path.module}/scripts/install.sh")}"
+
+  network_interface {
+    network = "${google_compute_network.google_vpc_network.self_link}"
+
+    access_config {}
+  }
+
+}
+
+resource "google_compute_instance" "google_west_cockroach" {
+
+  count = 3
+
+  name = "crdb-gcp-west-${count.index}"
+  machine_type = "n1-standard-4"
+  provider = "google.west"
+
+  tags = ["cockroach"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+      size = "350"
+    }
+  }
+
+  scratch_disk {}
+
+  metadata_startup_script = "${file("${path.module}/scripts/install.sh")}"
+
+  network_interface {
+    network = "${google_compute_network.google_vpc_network.self_link}"
+
+    access_config {}
+  }
+
+}
+
+locals {
+  google_public_ips = "${concat(google_compute_instance.google_east_cockroach.*.network_interface.0.access_config.0.nat_ip, google_compute_instance.google_west_cockroach.*.network_interface.0.access_config.0.nat_ip)}"
+  google_private_ips = "${concat(google_compute_instance.google_east_cockroach.*.network_interface.0.network_ip, google_compute_instance.google_west_cockroach.*.network_interface.0.network_ip)}"
+  google_dns_names = "${concat(google_compute_instance.google_east_cockroach.*.name, google_compute_instance.google_west_cockroach.*.name)}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# start gcp cluster
+# ---------------------------------------------------------------------------------------------------------------------
+
+# todo: think i need to create a master list of instances and use that here
+
+resource "null_resource" "google_start_cluster" {
+
+  # todo turn into variable
+  count = 6
 
   depends_on = ["google_compute_firewall.google_ssh"]
 
   connection {
     user = "timveil"
-    host = "${element(google_compute_instance.google_cockroach.*.network_interface.0.access_config.0.nat_ip, count.index)}"
+    host = "${element(local.google_public_ips, count.index)}"
     private_key = "${file("~/.ssh/google_compute_engine")}"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "cockroach start --insecure --cache=.25 --max-sql-memory=.25 --background --advertise-addr=${element(google_compute_instance.google_cockroach.*.network_interface.0.access_config.0.nat_ip, count.index)} --join=${join(",", google_compute_instance.google_cockroach.*.network_interface.0.access_config.0.nat_ip)}",
+      "cockroach start --insecure --cache=.25 --max-sql-memory=.25 --background --advertise-addr=${element(local.google_public_ips, count.index)} --join=${join(",", local.google_public_ips)}",
       "sleep 20"
     ]
   }
 
 }
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# init gcp cluster
+# ---------------------------------------------------------------------------------------------------------------------
 
 resource "null_resource" "google_init_cluster" {
 
@@ -101,7 +187,7 @@ resource "null_resource" "google_init_cluster" {
 
   connection {
     user = "timveil"
-    host = "${google_compute_instance.google_cockroach.0.network_interface.0.access_config.0.nat_ip}"
+    host = "${element(local.google_public_ips, 0)}"
     private_key = "${file("~/.ssh/google_compute_engine")}"
   }
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "google_cockroach_public_ips" {
   description = "Public IP's of Cockroach Nodes"
-  value = "${join(",", google_compute_instance.google_cockroach.*.network_interface.0.access_config.0.nat_ip)}"
+  value = "${join(",", local.google_public_ips)}"
 }
 
 output "google_cockroach_private_ips" {
   description = "Private IP's of Cockroach Nodes"
-  value = "${join(",", google_compute_instance.google_cockroach.*.network_interface.0.network_ip)}"
+  value = "${join(",", local.google_private_ips)}"
 }
 
 output "google_cockroach_instances" {
   description = "Names of Cockroach Nodes"
-  value = "${join(",", google_compute_instance.google_cockroach.*.name)}"
+  value = "${join(",", local.google_dns_names)}"
 }
 
 output "google_admin_urls" {
   description = "Admin URL's for Cockroach Nodes"
-  value = "${join(",", formatlist("http://%s:8080/", google_compute_instance.google_cockroach.*.network_interface.0.access_config.0.nat_ip))}"
+  value = "${join(",", formatlist("http://%s:8080/", local.google_public_ips))}"
 }
