0421825864407a3c3d6d4e35894c8e8237c910f0
Tim Veil <tjveil@gmail.com>
2019-02-27 21:38:14 -0500

    azure is almost working but not yet

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,17 +39,25 @@ provider "google" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# setup gcp network
+# setup azure provider
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "google_compute_network" "google_vpc_network" {
+provider "azurerm" {
+}
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# provision gcp resources
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_compute_network" "sd_compute_network" {
   name = "crdb-network"
   auto_create_subnetworks = "true"
 }
 
-resource "google_compute_firewall" "google_sql" {
+resource "google_compute_firewall" "sd_sql" {
   name = "allow-crdb-sql"
-  network = "${google_compute_network.google_vpc_network.self_link}"
+  network = "${google_compute_network.sd_compute_network.self_link}"
 
   allow {
     protocol = "tcp"
@@ -59,9 +67,9 @@ resource "google_compute_firewall" "google_sql" {
   target_tags = ["cockroach"]
 }
 
-resource "google_compute_firewall" "google_ui" {
+resource "google_compute_firewall" "sd_ui" {
   name = "allow-crdb-ui"
-  network = "${google_compute_network.google_vpc_network.self_link}"
+  network = "${google_compute_network.sd_compute_network.self_link}"
 
   allow {
     protocol = "tcp"
@@ -71,9 +79,9 @@ resource "google_compute_firewall" "google_ui" {
   target_tags = ["cockroach"]
 }
 
-resource "google_compute_firewall" "google_ssh" {
+resource "google_compute_firewall" "sd_ssh" {
   name = "allow-ssh"
-  network = "${google_compute_network.google_vpc_network.self_link}"
+  network = "${google_compute_network.sd_compute_network.self_link}"
 
   allow {
     protocol = "tcp"
@@ -83,11 +91,7 @@ resource "google_compute_firewall" "google_ssh" {
   target_tags = ["cockroach"]
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# provision gcp resources
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "google_compute_instance" "google_east_cockroach" {
+resource "google_compute_instance" "sd_east_cockroach_node" {
 
   count = 3
 
@@ -109,14 +113,14 @@ resource "google_compute_instance" "google_east_cockroach" {
   metadata_startup_script = "${file("${path.module}/scripts/install-crdb.sh")}"
 
   network_interface {
-    network = "${google_compute_network.google_vpc_network.self_link}"
+    network = "${google_compute_network.sd_compute_network.self_link}"
 
     access_config {}
   }
 
 }
 
-resource "google_compute_instance" "google_east_client" {
+resource "google_compute_instance" "sd_east_client" {
 
   name = "crdb-gcp-east-client"
   machine_type = "n1-standard-4"
@@ -134,14 +138,14 @@ resource "google_compute_instance" "google_east_client" {
   metadata_startup_script = "${file("${path.module}/scripts/install-client.sh")}"
 
   network_interface {
-    network = "${google_compute_network.google_vpc_network.self_link}"
+    network = "${google_compute_network.sd_compute_network.self_link}"
 
     access_config {}
   }
 
 }
 
-resource "google_compute_instance" "google_west_cockroach" {
+resource "google_compute_instance" "sd_west_cockroach_node" {
 
   count = 3
 
@@ -163,14 +167,14 @@ resource "google_compute_instance" "google_west_cockroach" {
   metadata_startup_script = "${file("${path.module}/scripts/install-crdb.sh")}"
 
   network_interface {
-    network = "${google_compute_network.google_vpc_network.self_link}"
+    network = "${google_compute_network.sd_compute_network.self_link}"
 
     access_config {}
   }
 
 }
 
-resource "google_compute_instance" "google_west_client" {
+resource "google_compute_instance" "sd_west_client" {
 
   name = "crdb-gcp-west-client"
   machine_type = "n1-standard-4"
@@ -188,9 +192,158 @@ resource "google_compute_instance" "google_west_client" {
   metadata_startup_script = "${file("${path.module}/scripts/install-client.sh")}"
 
   network_interface {
-    network = "${google_compute_network.google_vpc_network.self_link}"
+    network = "${google_compute_network.sd_compute_network.self_link}"
 
     access_config {}
+  }
+
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# provision azure resources
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+resource "azurerm_resource_group" "sd_resource_group" {
+  name = "sd-resource-group"
+  location = "centralus"
+}
+
+resource "azurerm_virtual_network" "sd_virtual_network" {
+  name = "sd-virtual-network"
+  address_space = ["10.0.0.0/16"]
+  location = "centralus"
+  resource_group_name = "${azurerm_resource_group.sd_resource_group.name}"
+}
+
+resource "azurerm_subnet" "sd_subnet" {
+  name = "sd-subnet"
+  resource_group_name = "${azurerm_resource_group.sd_resource_group.name}"
+  virtual_network_name = "${azurerm_virtual_network.sd_virtual_network.name}"
+  address_prefix = "10.0.2.0/24"
+}
+
+resource "azurerm_public_ip" "sd_public_ip" {
+  count = 3
+
+  name = "sd-public-ip-${count.index}"
+  location = "centralus"
+  resource_group_name = "${azurerm_resource_group.sd_resource_group.name}"
+  allocation_method = "Dynamic"
+}
+
+data "azurerm_public_ip" "sd_public_ip" {
+  count = 3
+
+  name = "${element(azurerm_public_ip.sd_public_ip.*.name, count.index)}"
+  resource_group_name = "${azurerm_resource_group.sd_resource_group.name}"
+}
+
+
+resource "azurerm_network_security_group" "sd_security_group" {
+  name = "sd-network-security-group"
+  location = "centralus"
+  resource_group_name = "${azurerm_resource_group.sd_resource_group.name}"
+
+  security_rule {
+    name = "SSH"
+    priority = 1001
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name = "DB"
+    priority = 1002
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "26257"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
+
+  security_rule {
+    name = "UI"
+    priority = 1003
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "8080"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "random_id" "sd_randomId" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = "${azurerm_resource_group.sd_resource_group.name}"
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_network_interface" "sd_network_interface" {
+  count = 3
+
+  name = "sd-network-interface-${count.index}"
+  location = "centralus"
+  resource_group_name = "${azurerm_resource_group.sd_resource_group.name}"
+  network_security_group_id = "${azurerm_network_security_group.sd_security_group.id}"
+
+  ip_configuration {
+    name = "sd-network-interface-config-${count.index}"
+    subnet_id = "${azurerm_subnet.sd_subnet.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id = "${element(azurerm_public_ip.sd_public_ip.*.id, count.index)}"
+  }
+}
+
+resource "azurerm_virtual_machine" "sd_cockroach_node" {
+  count = 3
+
+  name = "sd-azure-central-${count.index}"
+
+  location = "centralus"
+  resource_group_name = "${azurerm_resource_group.sd_resource_group.name}"
+  network_interface_ids = ["${element(azurerm_network_interface.sd_network_interface.*.id, count.index)}"]
+  vm_size = "Standard_D13_v2"
+
+  storage_os_disk {
+    name = "sd_os_disk-${count.index}"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    disk_size_gb = 350
+  }
+
+  storage_image_reference {
+    publisher = "credativ"
+    offer = "Debian"
+    sku = "9"
+    version = "latest"
+  }
+
+  os_profile {
+    computer_name = "sd-azure-central-${count.index}"
+    admin_username = "azureuser"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path = "/home/azureuser/.ssh/authorized_keys"
+      key_data = "${file("~/.ssh/id_rsa.pub")}"
+    }
   }
 
 }
@@ -200,9 +353,13 @@ resource "google_compute_instance" "google_west_client" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 locals {
-  google_public_ips = "${concat(google_compute_instance.google_east_cockroach.*.network_interface.0.access_config.0.nat_ip, google_compute_instance.google_west_cockroach.*.network_interface.0.access_config.0.nat_ip)}"
-  google_private_ips = "${concat(google_compute_instance.google_east_cockroach.*.network_interface.0.network_ip, google_compute_instance.google_west_cockroach.*.network_interface.0.network_ip)}"
-  google_dns_names = "${concat(google_compute_instance.google_east_cockroach.*.name, google_compute_instance.google_west_cockroach.*.name)}"
+  # Google
+  google_public_ips = "${concat(google_compute_instance.sd_east_cockroach_node.*.network_interface.0.access_config.0.nat_ip, google_compute_instance.sd_west_cockroach_node.*.network_interface.0.access_config.0.nat_ip)}"
+  google_private_ips = "${concat(google_compute_instance.sd_east_cockroach_node.*.network_interface.0.network_ip, google_compute_instance.sd_west_cockroach_node.*.network_interface.0.network_ip)}"
+  google_dns_names = "${concat(google_compute_instance.sd_east_cockroach_node.*.name, google_compute_instance.sd_west_cockroach_node.*.name)}"
+
+  # Azure
+  azure_public_ips = "${data.azurerm_public_ip.sd_public_ip.*.ip_address}"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -211,10 +368,12 @@ locals {
 
 resource "null_resource" "google_start_cluster" {
 
-  # todo turn into variable
   count = 6
 
-  depends_on = ["google_compute_firewall.google_ssh"]
+  depends_on = [
+    "google_compute_firewall.sd_ssh",
+    "google_compute_instance.sd_east_cockroach_node",
+    "google_compute_instance.sd_west_cockroach_node"]
 
   connection {
     user = "timveil"
@@ -231,16 +390,42 @@ resource "null_resource" "google_start_cluster" {
 
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# install azure cluster
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+resource "null_resource" "azure_install_cluster" {
+
+  count = 3
+
+  depends_on = ["azurerm_virtual_machine.sd_cockroach_node", "null_resource.google_start_cluster"]
+
+  connection {
+    user = "timveil"
+    host = "${element(local.azure_public_ips, count.index)}"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    timeout = "30s"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget -qO- https://binaries.cockroachdb.com/cockroach-v2.1.5.linux-amd64.tgz | tar  xvz",
+      "sudo cp -i cockroach-v2.1.5.linux-amd64/cockroach /usr/local/bin",
+      "cockroach start --insecure --cache=.25 --max-sql-memory=.25 --background --advertise-addr=${element(local.azure_public_ips, count.index)} --join=${join(",", local.google_public_ips)}",
+      "sleep 20"
+    ]
+  }
+
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
-# init gcp cluster
+# init global cluster
 # ---------------------------------------------------------------------------------------------------------------------
 
-# todo this becomes generic; depends on azure and google but will call into google to start
+resource "null_resource" "global_init_cluster" {
 
-resource "null_resource" "google_init_cluster" {
-
-  depends_on = ["null_resource.google_start_cluster"]
+  depends_on = ["null_resource.google_start_cluster", "null_resource.azure_install_cluster"]
 
   connection {
     user = "timveil"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,19 @@
 output "google_cockroach_public_ips" {
-  description = "Public IP's of Cockroach Nodes"
+  description = "Public IP's of Cockroach Nodes in GCP"
   value = "${join(",", local.google_public_ips)}"
+}
+output "azure_cockroach_public_ips" {
+  description = "Public IP's of Cockroach Nodes in Azure"
+  value = "${join(",", local.azure_public_ips)}"
 }
 
 output "google_cockroach_private_ips" {
-  description = "Private IP's of Cockroach Nodes"
+  description = "Private IP's of Cockroach Nodes in GCP"
   value = "${join(",", local.google_private_ips)}"
 }
 
 output "google_cockroach_instances" {
-  description = "Names of Cockroach Nodes"
+  description = "Names of Cockroach Nodes in GCP"
   value = "${join(",", local.google_dns_names)}"
 }
 
