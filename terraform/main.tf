c343e501da88ac3eeeed1827c87d393db2ff72fd
Tim Veil <tjveil@gmail.com>
2019-03-01 23:08:18 -0500

    fixing schema

--- terraform/main.tf
+++ terraform/main.tf
@@ -581,6 +581,9 @@ resource "null_resource" "global_init_cluster" {
       "cockroach sql --insecure --execute=\"SET CLUSTER SETTING cluster.organization = '${var.crdb_license_org}';\"",
       "cockroach sql --insecure --execute=\"SET CLUSTER SETTING enterprise.license = '${var.crdb_license_key}';\"",
       "cockroach sql --insecure --execute=\"CREATE DATABASE store_demo;\"",
+      "cockroach sql --insecure --database=store_demo --execute=\"INSERT into system.locations VALUES ('region', 'southcentralus', 29.4167, -98.5);\"",
+      "cockroach sql --insecure --database=store_demo --execute=\"INSERT into system.locations VALUES ('region', 'us-east1', 33.191333, -80.003999);\"",
+      "cockroach sql --insecure --database=store_demo --execute=\"INSERT into system.locations VALUES ('region', 'us-west2', 34.052235, -118.243683);\"",
       "cockroach sql --insecure --database=store_demo < schema.sql"
     ]
   }
