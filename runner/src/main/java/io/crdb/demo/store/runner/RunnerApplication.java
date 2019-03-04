ec1c14ac0d36911f747e44861cf95a5015b8f4ec
Tim Veil <tjveil@gmail.com>
2019-03-04 12:03:59 -0500

    fixing dashboard

--- runner/src/main/java/io/crdb/demo/store/runner/RunnerApplication.java
+++ runner/src/main/java/io/crdb/demo/store/runner/RunnerApplication.java
@@ -1,13 +1,22 @@
 package io.crdb.demo.store.runner;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RunnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerApplication.class, args);
+	}
+
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", "runner");
 	}
 
 }
