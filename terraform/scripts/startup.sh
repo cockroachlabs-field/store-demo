23b62e325162c8bfb8dcf5b7029c719e8815758e
Tim Veil <tjveil@gmail.com>
2019-03-05 13:12:46 -0500

    fixing ntp

--- terraform/scripts/startup.sh
+++ terraform/scripts/startup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+sudo timedatectl set-ntp no
+
+sudo apt-get install ntp ntpstat
+
+sudo service ntp stop
+
+sudo ntpd -b time.google.com
+
+sudo sed -i '/^pool/s/^/#/' /etc/ntp.conf
+sudo sed -i '/^server/s/^/#/' /etc/ntp.conf
+
+sudo tee -a /etc/ntp.conf <<EOL
+server time1.google.com iburst
+server time2.google.com iburst
+server time3.google.com iburst
+server time4.google.com iburst
+EOL
+
+sudo service ntp start
+
 # Increase Limits
 sudo tee -a /etc/security/limits.conf <<EOL
 *              soft     nofile          66000
