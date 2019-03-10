f0e09005a9a0277265c117ef8a9c866bffd96200
Tim Veil <tjveil@gmail.com>
2019-03-10 14:43:46 -0400

    adding lease preferences

--- loader/src/main/resources/zone-config.sql
+++ loader/src/main/resources/zone-config.sql
@@ -1,6 +1,6 @@
-ALTER PARTITION east OF TABLE ACCT CONFIGURE ZONE USING constraints='[+region=east]';
-ALTER PARTITION central OF TABLE ACCT CONFIGURE ZONE USING constraints='[+region=central]';
-ALTER PARTITION west OF TABLE ACCT CONFIGURE ZONE USING constraints='[+region=west]';
-ALTER PARTITION east OF TABLE AUTH CONFIGURE ZONE USING constraints='[+region=east]';
-ALTER PARTITION central OF TABLE AUTH CONFIGURE ZONE USING constraints='[+region=central]';
-ALTER PARTITION west OF TABLE AUTH CONFIGURE ZONE USING constraints='[+region=west]';
+ALTER PARTITION east OF TABLE ACCT CONFIGURE ZONE USING constraints='[+region=east]', lease_preferences='[[+region=east]]';
+ALTER PARTITION central OF TABLE ACCT CONFIGURE ZONE USING constraints='[+region=central]', lease_preferences='[[+region=central]]';
+ALTER PARTITION west OF TABLE ACCT CONFIGURE ZONE USING constraints='[+region=west]', lease_preferences='[[+region=west]]';
+ALTER PARTITION east OF TABLE AUTH CONFIGURE ZONE USING constraints='[+region=east]', lease_preferences='[[+region=east]]';
+ALTER PARTITION central OF TABLE AUTH CONFIGURE ZONE USING constraints='[+region=central]', lease_preferences='[[+region=central]]';
+ALTER PARTITION west OF TABLE AUTH CONFIGURE ZONE USING constraints='[+region=west]', lease_preferences='[[+region=west]]';
