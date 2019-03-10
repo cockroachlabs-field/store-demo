df0f576f2e2c0bbd39d40b135395f007073ad32c
Tim Veil <tjveil@gmail.com>
2019-03-09 22:14:25 -0500

    adding some echo

--- terraform/scripts/startup.sh
+++ terraform/scripts/startup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+echo "disable timedatectl"
 sudo timedatectl set-ntp no
 
+echo "install npt"
 sudo apt-get update -yq
 sudo apt-get install -yq ntp ntpstat
 
+echo "stop ntp"
 sudo service ntp stop
+
+echo "calling ntpd -b"
 sudo ntpd -b time.google.com
 
+echo "sed on ntp.conf"
 sudo sed -i '/^pool/s/^/#/' /etc/ntp.conf
 sudo sed -i '/^server/s/^/#/' /etc/ntp.conf
 
+echo "append to ntp.conf"
 sudo tee -a /etc/ntp.conf <<EOL
 server time1.google.com iburst
 server time2.google.com iburst
@@ -18,9 +25,10 @@ server time3.google.com iburst
 server time4.google.com iburst
 EOL
 
+echo "start ntp"
 sudo service ntp start
 
-# Increase Limits
+echo "increase limits"
 sudo tee -a /etc/security/limits.conf <<EOL
 *              soft     nofile          66000
 *              hard     nofile          66000
