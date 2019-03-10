ae510bf4d6f197fef9b372dd2d72e6fb9873b232
Tim Veil <tjveil@gmail.com>
2019-03-09 22:24:54 -0500

    writing stdout to txt file

--- terraform/plan.sh
+++ terraform/plan.sh
@@ -3,6 +3,6 @@
 export TF_LOG="DEBUG"
 export TF_LOG_PATH="plan.log"
 
-rm -rf plan.log plan.out
+rm -rf plan.log plan.txt
 
-terraform plan -var-file="store-demo.tfvars" 2>&1 | tee plan.out
+terraform plan -var-file="store-demo.tfvars" 2>&1 | tee plan.txt
