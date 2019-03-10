b228b68ab7b43b46c32dacacbec263563bdd7379
Tim Veil <tjveil@gmail.com>
2019-03-09 22:22:35 -0500

    writing stdout to file

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -3,6 +3,6 @@
 export TF_LOG="DEBUG"
 export TF_LOG_PATH="apply.log"
 
-rm -rf apply.log
+rm -rf apply.log apply.out
 
-terraform apply -var-file="store-demo.tfvars" -auto-approve
+terraform apply -var-file="store-demo.tfvars" -auto-approve 2>&1 | tee apply.out
--- terraform/destroy.sh
+++ terraform/destroy.sh
@@ -3,6 +3,6 @@
 export TF_LOG="DEBUG"
 export TF_LOG_PATH="destroy.log"
 
-rm -rf destroy.log
+rm -rf destroy.log destroy.out
 
-terraform destroy -var-file="store-demo.tfvars" -auto-approve
+terraform destroy -var-file="store-demo.tfvars" -auto-approve 2>&1 | tee destroy.out
