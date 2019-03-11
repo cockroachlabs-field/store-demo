8a18c6d0797ef6862c8c63eb260ae681c8106f94
Tim Veil <tjveil@gmail.com>
2019-03-11 10:28:29 -0400

    removing echos

--- terraform/scripts/startup.sh
+++ terraform/scripts/startup.sh
@@ -2,25 +2,19 @@
 
 while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 1; done
 
-echo "install npt"
 sudo apt-get update --fix-missing
 sudo apt-get upgrade -y
 sudo apt-get install -yqq ntp ntpstat
 
-echo "disable timedatectl"
 sudo timedatectl set-ntp no
 
-echo "stop ntp"
 sudo service ntp stop
 
-echo "calling ntpd -b"
 sudo ntpd -b time.google.com
 
-echo "sed on ntp.conf"
 sudo sed -i '/^pool/s/^/#/' /etc/ntp.conf
 sudo sed -i '/^server/s/^/#/' /etc/ntp.conf
 
-echo "append to ntp.conf"
 sudo tee -a /etc/ntp.conf <<EOL
 server time1.google.com iburst
 server time2.google.com iburst
@@ -28,10 +22,8 @@ server time3.google.com iburst
 server time4.google.com iburst
 EOL
 
-echo "start ntp"
 sudo service ntp start
 
-echo "increase limits"
 sudo tee -a /etc/security/limits.conf <<EOL
 *              soft     nofile          66000
 *              hard     nofile          66000
