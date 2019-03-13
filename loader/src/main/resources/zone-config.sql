ce19616f752d5f1dbba10ffb879d65bdaeaee03d
Tim Veil <tjveil@gmail.com>
2019-03-13 15:46:24 -0400

    missing auth config

--- loader/src/main/resources/zone-config.sql
+++ loader/src/main/resources/zone-config.sql
@@ -4,3 +4,10 @@ ALTER PARTITION C OF TABLE ACCT CONFIGURE ZONE USING gc.ttlseconds=600, constrai
 ALTER PARTITION D OF TABLE ACCT CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=C": 1, "+region=D": 1, "+region=B": 1}', lease_preferences='[[+region=D]]';
 ALTER PARTITION D OF TABLE ACCT CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=E": 1, "+region=F": 1, "+region=C": 1}', lease_preferences='[[+region=E]]';
 ALTER PARTITION F OF TABLE ACCT CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=E": 1, "+region=F": 1, "+region=D": 1}', lease_preferences='[[+region=F]]';
+
+ALTER PARTITION A OF TABLE AUTH CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=A": 1, "+region=B": 1, "+region=C": 1}', lease_preferences='[[+region=A]]';
+ALTER PARTITION B OF TABLE AUTH CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=A": 1, "+region=B": 1, "+region=D": 1}', lease_preferences='[[+region=B]]';
+ALTER PARTITION C OF TABLE AUTH CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=C": 1, "+region=D": 1, "+region=A": 1}', lease_preferences='[[+region=C]]';
+ALTER PARTITION D OF TABLE AUTH CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=C": 1, "+region=D": 1, "+region=B": 1}', lease_preferences='[[+region=D]]';
+ALTER PARTITION D OF TABLE AUTH CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=E": 1, "+region=F": 1, "+region=C": 1}', lease_preferences='[[+region=E]]';
+ALTER PARTITION F OF TABLE AUTH CONFIGURE ZONE USING gc.ttlseconds=600, constraints='{"+region=E": 1, "+region=F": 1, "+region=D": 1}', lease_preferences='[[+region=F]]';
